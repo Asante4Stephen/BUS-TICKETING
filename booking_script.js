a231ca67bb56af103600e443579a677cdef196b5
Asante4Stephen <kwabenastephenasante2019@gmail.com>
2023-11-09 13:42:14 +0000

    new

--- booking_script.js
+++ booking_script.js
@@ -1,74 +1,117 @@
- // Sample bus data
- const buses = [
-    {
-        name: 'Bus A',
-        company: 'Company A',
-        location: 'Location A',
-        seats: [
-            { number: 1, isBooked: false },
-            { number: 2, isBooked: false },
-            // ... more seats ...
-        ],
-    },
 
-    
-    // ... more buses ...
-];
 
+
+// JavaScript code for fetching bus data and handling user interactions
 function searchBuses() {
-    // Perform your search logic here
-    // For simplicity, this example assumes that all buses match the search criteria
+    // Fetch available buses based on user input
+    const fullname = document.getElementById('fullName').value;
+    const companies = document.getElementById('companies').value;
+    const location = document.getElementById('location').value;
+    const passenger = document.getElementById('passenger').value;
+    const depDate = document.getElementById('depDate').value;
+    const depTime = document.getElementById('depTime').value;
+    console.log(location)
 
-    const searchResults = document.getElementById('bus-search-results');
-    searchResults.innerHTML = '';
+    // Perform an API request to get bus data and display results
+    // You'd typically connect to a server for this.
 
-    buses.forEach((bus) => {
-        const busElement = document.createElement('div');
-        busElement.innerHTML = `
-            <h2>${bus.name}</h2>
-            <p>Company: ${bus.company}</p>
-            <p>Location: ${bus.location}</p>
-            <button onclick="selectBus('${bus.name}')">Select Bus</button>
-        `;
-        searchResults.appendChild(busElement);
-    });
+    // Example: Display available buses
+    const busSearchResults = document.getElementById('bus-search-results');
+    busSearchResults.innerHTML = `<h2>Available Buses</h2>
+        <ul>
+            <li>Bus A - Price: $50 <button onclick="selectSeats('Bus A')">Select Seats</button></li>
+            <li>Bus B - Price: $60 <button onclick="selectSeats('Bus B')">Select Seats</button></li>
+            <li>Bus C - Price: $70 <button onclick="selectSeats('Bus C')">Select Seats</button></li>
+        </ul>`;
+        busSearchResults.style.display = 'block';;
+        busSearchResults.style.margin = 'auto';
+        busSearchResults.style.padding = '10px';
+        busSearchResults.style.border = '1px solid black';
+        busSearchResults.style.background = 'white';
+        busSearchResults.style.borderRadius = '10px';
+        
+            // <li>Bus A - Price: $50 <button onclick="selectSeats('Bus A')">Select Seats</button></li>
 }
 
-// function searchBus() {
-//     // ... perform your search logic here ...
+// Function to handle seat selection
+function selectSeats(busName) {
+    const busSearchResults = document.getElementById('bus-search-results');
+    busSearchResults.style.display = 'block';
 
-//     // Assuming 'results' is the data returned from your search function
-//     const results = ['Result 1', 'Result 2', 'Result 3'];
-
-//     // Update the 'results' div with the search results
-//     const resultsDiv = document.getElementById('results');
-//     resultsDiv.innerHTML = '';
-//     results.forEach((result) => {
-//         const resultElement = document.createElement('p');
-//         resultElement.textContent = result;
-//         resultsDiv.appendChild(resultElement);
-//     });
-
-//     // Show the results
-//     document.querySelector('.container').classList.add('active');
-
-function selectBus(busName) {
-    // Find the selected bus in the buses array
-    const selectedBus = buses.find((bus) => bus.name === busName);
-
-    if (!selectedBus) {
-        alert('Error: Bus not found');
-        return;
-    }
-
-    // Display the seat layout for the selected bus
     const seatSelection = document.getElementById('seat-selection');
-    seatSelection.innerHTML = '';
+    seatSelection.innerHTML = `<h2>Seat Selection for ${busName}</h2>
+    <p>Select your seats:</p>
+    <select id="selected-seats" multiple>
+    <option value="1A">1A</option>
+    <option value="1B">1B</option>
+    <option value="2A">2A</option>
+    <option value="2B">2B</option>
+    <!-- Add more seat options -->
+    </select>
+    <button onclick="proceedToPayment('${busName}')" 
+    style="color : black;
+    border: groove black 2px
+    " >Proceed to Payment</button>`;
+    seatSelection.style.display = 'block';
+    seatSelection.style.margin = 'auto';
+    seatSelection.style.padding = '10px';
+    seatSelection.style.border = '1px solid black';
+    seatSelection.style.background = 'white';
+    seatSelection.style.borderRadius = '10px';
+    
+        // <p>Select your seats:</p>
+        // <select id="selected-seats" multiple>
+        //     <option value="1A">1A</option>
+        //     <option value="1B">1B</option>
+        //     <option value="2A">2A</option>
+        //     <option value="2B">2B</option>
+        //     <!-- Add more seat options -->
+        // </select>
 
-    selectedBus.seats.forEach((seat) => {
-        const seatElement = document.createElement('div');
-        seatElement.className = seat.isBooked ? 'seat-booked' : 'seat-available';
-        seatElement.innerHTML = `<p>Seat ${seat.number}</p>`;
-        seatSelection.appendChild(seatElement);
-    });
+        // <button onclick="proceedToPayment('${busName}')">Proceed to Payment</button>`;
 }
+
+// Function to handle payment options
+function proceedToPayment(busName) {
+    const seatSelection = document.getElementById('seat-selection');
+    seatSelection.style.display = 'block';
+
+    const paymentOptions = document.getElementById('payment-options');
+    paymentOptions.innerHTML = `<h2>Payment Options</h2>
+        <p>Choose a payment method:</p>
+        <select id="payment-method">
+            <option value="credit-card">Credit Card</option>
+            <option value="debit-card">Debit Card</option>
+            <option value="wallet">Mobile Wallet</option>
+            <option value="online-banking">Online Banking</option>
+        </select>
+        <button onclick="confirmBooking('${busName}')" style="color : black;
+        border: groove black 2px
+        "
+        >Confirm Booking</button>`;
+}
+
+// Function to confirm the booking
+function confirmBooking(busName) {
+    const paymentOptions = document.getElementById('payment-options');
+    paymentOptions.style.display = 'block';
+
+    // You can implement actual payment processing here
+    // Generate e-tickets and send booking confirmation
+
+    const confirmationMessage = document.createElement('div');
+    confirmationMessage.innerHTML = `<h2>Booking Confirmation</h2>
+        <p>Your booking for ${busName} has been confirmed.</p>
+        <p>An e-ticket has been sent to your email.</p>`;
+    
+    document.body.appendChild(confirmationMessage);
+}
+
+
+
+
+
+
+
+
+
