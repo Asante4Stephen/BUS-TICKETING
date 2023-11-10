a347cd5b99c09ba2735c362c0615aebbccb314fd
Asante4Stephen <kwabenastephenasante2019@gmail.com>
2023-11-10 09:11:53 +0000

    new

--- booking_script.js
+++ booking_script.js
@@ -12,9 +12,11 @@ function searchBuses() {
 
     // Perform an API request to get bus data and display results
     // You'd typically connect to a server for this.
+    window.location.href="1.html"
 
     // Example: Display available buses
     const busSearchResults = document.getElementById('bus-search-results');
+
     busSearchResults.innerHTML = `<h2>Available Buses</h2>
         <ul>
             <li>Bus A - Price: $50 <button onclick="selectSeats('Bus A')">Select Seats</button></li>
@@ -28,42 +30,68 @@ function searchBuses() {
         busSearchResults.style.background = 'white';
         busSearchResults.style.borderRadius = '10px';
 
-        window.location.href="/1.html"
-
         
             // <li>Bus A - Price: $50 <button onclick="selectSeats('Bus A')">Select Seats</button></li>
 }
 
 // Function to handle seat selection
 function selectSeats(busName) {
-    const busSearchResults = document.getElementById('bus-search-results');
-    busSearchResults.style.display = 'block';
-    busSearchResults.style.margin = 'auto';
-    busSearchResults.style.padding = '10px';
-    busSearchResults.style.border = '1px solid black';
-    busSearchResults.style.background = 'white';
-    busSearchResults.style.borderRadius = '10px';
-
+    
+    window.location.href="2.html"
+    
+    
     const seatSelection = document.getElementById('seat-selection');
-    seatSelection.innerHTML = `<h2>Seat Selection for ${busName}</h2>
-    <p>Select your seats:</p>
-    <select id="selected-seats" multiple>
-    <option value="1A">1A</option>
-    <option value="1B">1B</option>
-    <option value="2A">2A</option>
-    <option value="2B">2B</option>
-    <!-- Add more seat options -->
-    </select>
-    <button onclick="proceedToPayment('${busName}')" 
-    style="color : black;
-    border: groove black 2px
-    " >Proceed to Payment</button>`;
-    seatSelection.style.display = 'block';
-    seatSelection.style.margin = 'auto';
-    seatSelection.style.padding = 'auto';
-    seatSelection.style.border = '1px solid black';
-    seatSelection.style.background = 'white';
-    seatSelection.style.borderRadius = '10px';
+    // seatSelection.innerHTML = `<h2>Seat Selection for </h2>
+    //     <p>Select your seats:</p>
+    //     <select id="selected-seats" multiple>
+    //     <option value="1A">1A</option>
+    //     <option value="1B">1B</option>
+    //     <option value="2A">2A</option>
+    //     <option value="2B">2B</option>
+    //     <!-- Add more seat options -->
+    //     </select>
+    //     <button onclick="proceedToPayment('$}')" 
+    //     style="color : black;
+    //     border: groove black 2px
+    //     " >Proceed to Payment</button>`;
+    //     seatSelection.style.display = 'block';
+    //     seatSelection.style.margin = 'auto';
+    //     seatSelection.style.padding = 'auto';
+    //     seatSelection.style.border = '1px solid black';
+
+    //     seatSelection.style.background = 'white';
+    //     seatSelection.style.borderRadius = '10px';
+
+    // const busSearchResults = document.getElementById('bus-search-results');
+    // busSearchResults.style.display = 'block';
+    // busSearchResults.style.margin = 'auto';
+    // busSearchResults.style.padding = '10px';
+    // busSearchResults.style.border = '1px solid black';
+    // busSearchResults.style.background = 'white';
+    // busSearchResults.style.borderRadius = '10px';
+
+
+
+    // const seatSelection = document.getElementById('seat-selection');
+    // seatSelection.innerHTML = `<h2>Seat Selection for ${busName}</h2>
+    // <p>Select your seats:</p>
+    // <select id="selected-seats" multiple>
+    // <option value="1A">1A</option>
+    // <option value="1B">1B</option>
+    // <option value="2A">2A</option>
+    // <option value="2B">2B</option>
+    // <!-- Add more seat options -->
+    // </select>
+    // <button onclick="proceedToPayment('${busName}')" 
+    // style="color : black;
+    // border: groove black 2px
+    // " >Proceed to Payment</button>`;
+    // seatSelection.style.display = 'block';
+    // seatSelection.style.margin = 'auto';
+    // seatSelection.style.padding = 'auto';
+    // seatSelection.style.border = '1px solid black';
+    // seatSelection.style.background = 'white';
+    // seatSelection.style.borderRadius = '10px';
     
         // <p>Select your seats:</p>
         // <select id="selected-seats" multiple>
@@ -76,55 +104,67 @@ function selectSeats(busName) {
 
         // <button onclick="proceedToPayment('${busName}')">Proceed to Payment</button>`;
 
-        window.location.href="2.html"
         
 }
 
 // Function to handle payment options
 function proceedToPayment(busName) {
-    const seatSelection = document.getElementById('seat-selection');
-    seatSelection.style.display = 'block';
-    seatSelection.style.margin = 'auto';
-    seatSelection.style.padding = 'auto';
-    seatSelection.style.border = '1px solid black';
-    seatSelection.style.background = 'white';
-    seatSelection.style.borderRadius = '10px';
+    
+    window.location.href="3.html"
 
+    
     const paymentOptions = document.getElementById('payment-options');
-    paymentOptions.innerHTML = `<h2>Payment Options</h2>
-        <p>Choose a payment method:</p>
-        <select id="payment-method">
-            <option value="credit-card">Credit Card</option>
-            <option value="debit-card">Debit Card</option>
-            <option value="wallet">Mobile Wallet</option>
-            <option value="online-banking">Online Banking</option>
-        </select>
-        <button onclick="confirmBooking('${busName}')" style="color : black;
-        border: groove black 2px
-        "
-        >Confirm Booking</button>`;
-        paymentOptions.style.display = 'block';
-        
-        window.location.href="3.html"
-        
-        
+    // paymentOptions.innerHTML = `<h2>Payment Options</h2>
+    //     <p>Choose a payment method:</p>
+    //     <select id="payment-method">
+    //         <option value="credit-card">Credit Card</option>
+    //         <option value="debit-card">Debit Card</option>
+    //         <option value="wallet">Mobile Wallet</option>
+    //         <option value="online-banking">Online Banking</option>
+    //     </select>
+    //     <button onclick="confirmBooking('${busName}')" style="color : black;
+    //     border: groove black 2px
+    //     "
+    //     >Confirm Booking</button>`;
+    //     paymentOptions.style.display = 'block';
+    //     paymentOptions.style.margin = 'auto';
+    //     paymentOptions.style.padding = 'auto';
+    //     paymentOptions.style.border = '1px solid black';
+    //     paymentOptions.style.background = 'white';
+    //     paymentOptions.style.borderRadius = '10px';
+
+    // const seatSelection = document.getElementById('seat-selection');
+    // seatSelection.style.display = 'block';
+    // seatSelection.style.margin = 'auto';
+    // seatSelection.style.padding = 'auto';
+    // seatSelection.style.border = '1px solid black';
+    // seatSelection.style.background = 'white';
+    // seatSelection.style.borderRadius = '10px';
+
+   
 }
+
+// Function to confirm booking and display success message
+        
+        
+        
+
 
 // Function to confirm the booking
 function confirmBooking(busName) {
-    const paymentOptions = document.getElementById('confirmation');
-    paymentOptions.style.display = 'block';
+    
+    window.location.href="4.html"
+
 
     // You can implement actual payment processing here
     // Generate e-tickets and send booking confirmation
 
-    const confirmationMessage = document.createElement('div');
-    confirmationMessage.innerHTML = `<h2>Booking Confirmation</h2>
-        <p>Your booking for ${busName} has been confirmed.</p>
-        <p>An e-ticket has been sent to your email.</p>`;
+    // const confirmationMessage = document.getElementById('confirmation');
+    // confirmationMessage.innerHTML = `<h2>Booking Confirmation</h2>
+    //     <p>Your booking for ${busName} has been confirmed.</p>
+    //     <p>An e-ticket has been sent to your email.</p>`;
     
-    document.body.appendChild(confirmationMessage);
-    window.location.href="4.html"
+    // document.body.appendChild(confirmationMessage);
         
 }
 
